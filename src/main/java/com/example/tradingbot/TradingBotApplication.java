b5ff9fce21a46d8e6df1ece7871ea133e6766c68
Janek1010 <janekdomozych99@gmail.com>
2023-04-18 14:43:52 +0200

    init repo

---
+++ src/main/java/com/example/tradingbot/TradingBotApplication.java
@@ -0,0 +1,17 @@
+package com.example.tradingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingBotApplication.class, args);
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
+    }
+
+}
