560feee5e96e994709ef2b775ee33c0843213f10
Janek1010 <janekdomozych99@gmail.com>
2023-04-18 17:34:57 +0200

    home endpoint with home.html

--- src/main/java/com/example/tradingbot/TradingBotApplication.java
+++ src/main/java/com/example/tradingbot/TradingBotApplication.java
@@ -8,10 +8,7 @@ public class TradingBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TradingBotApplication.class, args);
-        System.out.println("aaa");
-        System.out.println("aaa");
-        System.out.println("aaa");
-        System.out.println("aaa");
+
     }
 
 }
